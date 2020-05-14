df5c360f13a667989bc780665508d59bfd7ccf90
eddie naff <eddienaff@eddienaff.com>
2020-05-14 12:02:23 -0700

    Add custom styling to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,66 @@
 import { NextPage, NextPageContext } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-import { getLayout } from "../components/layout.v2";
+import { getLayout } from "../components/entry-layout";
 import { withApollo } from "../lib/with-apollo_v2";
 import { NextPageStaticVariableProps } from "../typings/types";
-import { useHelloWorldQuery } from "../lib/queries/hello.graphql";
+import { Button, Flex, Text } from "../components/primitives/styled-rebass";
 
-interface IndexProps extends Partial<NextPageContext> {}
+interface IndexProps {}
 
-const Index: NextPage<IndexProps> & NextPageStaticVariableProps = (
-  {
-    // pathname,
-    // query
-  }
-) => {
-  const { data, error, loading } = useHelloWorldQuery();
-  if (data) {
-    const { helloWorld } = data;
-    return (
-      <div>
-        It's time to say it!!!
-        {helloWorld}
-      </div>
-    );
-  }
+const Index: NextPage<IndexProps, {}> & NextPageStaticVariableProps = ({}) => {
+  const router = useRouter();
+  return (
+    <Flex
+      flex={1}
+      width={1}
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Link href={`/?loginModal=isOpen`} as={`/login`}>
+        <a>Login</a>
+      </Link>
+      <Button
+        onClick={() => router.push("/?loginModal=isOpen", "/login")}
+        width={[4 / 5, "250px", "250px", "250px"]}
+        borderRadius={20}
+      >
+        <Text>Login</Text>
+      </Button>
+      <Flex
+        width={[4 / 5, "250px", "250px", "250px"]}
+        my={3}
+        flexDirection="row"
+        alignItems="center"
+      >
+        <Flex width={2 / 5} flexDirection="row" border="primary"></Flex>
+        <Flex
+          width={1 / 5}
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text>OR</Text>
+        </Flex>
 
-  if (loading) {
-    return <div>loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error {JSON.stringify(error, null, 2)}</div>;
-  }
-
-  return <div>IMPOSSIBLE STATE?</div>;
+        <Flex width={2 / 5} flexDirection="row" border="primary"></Flex>
+      </Flex>
+      <Button
+        onClick={() => {
+          router.push("/?registerModal=isOpen", "/register");
+        }}
+        width={[4 / 5, "250px", "250px", "250px"]}
+        borderRadius={20}
+      >
+        <Text>Create an account</Text>
+      </Button>
+    </Flex>
+  );
 };
 
-Index.getInitialProps = ({ pathname, query }) => {
+Index.getInitialProps = ({ pathname, query }: NextPageContext) => {
   return { pathname, query };
 };
 
