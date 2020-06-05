e75273097cebac4ad468e4436d893ce05ce1960e
eddie naff <eddienaff@eddienaff.com>
2020-06-05 14:12:10 -0700

    Adjust global styles

--- components/styles/global-styles.tsx
+++ components/styles/global-styles.tsx
@@ -4,10 +4,14 @@ export const GlobalStyles = createGlobalStyle`
 html {
   box-sizing: border-box;
   padding: 0;
+  height: 100%;
+  min-height: 100%;
   -webkit-tap-highlight-color: rgba(0,0,0,0);
   -webkit-tap-highlight-color: transparent;
 }
 body {
+  height: 100%;
+  min-height: 100%;
   margin: 0;
   padding: 0;
   text-size-adjust: 100%;
@@ -19,5 +23,5 @@ body {
   box-sizing: inherit;
 }
 
-#__next {  height: 100%; min-height: 100vh; display: flex; justify-content: center; }
+#__next {  height: 100%;  display: flex; justify-content: center; }
 `;
