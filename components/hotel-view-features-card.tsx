69216e7807771bed412d27a2769a5af8ed5cb7e2
eddie naff <eddienaff@eddienaff.com>
2020-06-09 17:36:41 -0700

    Embelish features card

--- components/hotel-view-features-card.tsx
+++ components/hotel-view-features-card.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 // import { CardProps } from "rebass/styled-components";
 
-import { Card } from "./primitives/styled-rebass";
+import { Card, Flex, Text } from "./primitives/styled-rebass";
 import { HotelViewCardProps } from "./hotel-view-modal";
-
-// interface HotelViewFeaturesCardProps {
-//   bg?: CardProps["bg"];
-// }
+import { featureList } from "./filter-features";
+import Icon from "./icon";
 
 export const HotelViewFeaturesCard: React.FC<HotelViewCardProps> = ({
   p,
@@ -21,7 +19,32 @@ export const HotelViewFeaturesCard: React.FC<HotelViewCardProps> = ({
         boxShadow: "0px 13px 13px 0px rgba(0, 0, 0, 0.05)",
       }}
     >
-      FEATURES / AMENITIES
+      <Flex alignItems="center" justifyContent="space-around" flexWrap="wrap">
+        {featureList.map((feature) => (
+          <Flex flexDirection="column" alignItems="center" p={3}>
+            <Flex
+              bg="rgba(94,104,112,.1)"
+              height="50px"
+              width="50px"
+              alignItems="center"
+              justifyContent="center"
+              pt={feature.name === "wifi" ? 3 : null}
+              sx={{
+                borderRadius: "50%",
+              }}
+            >
+              <Icon
+                key={feature.name}
+                active={false}
+                fill="rgba(94,104,112,1)"
+                name={feature.name}
+                size={feature.pixelSize}
+              />
+            </Flex>
+            <Text>{feature.label}</Text>
+          </Flex>
+        ))}
+      </Flex>
     </Card>
   );
 };
