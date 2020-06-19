f5c64ec249916cdbbd2cded2a971970974fe2828
eddie naff <eddienaff@eddienaff.com>
2020-06-18 17:54:22 -0700

    Add misguided attempt to fix scroll issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,6 +31,7 @@ class MyApp extends App<MyAppTooProps> {
         <GlobalStyles />
         <div
           id="modal"
+          style={{ maxHeight: "100%", overflowY: "auto" }}
           // style={{ position: "fixed", top: 0, right: 0, left: 0, bottom: 0 }}
         />
         <div
